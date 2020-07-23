1f1500727d632427fab76a625c04aad9415168b7
6174841226 <6174841226@163.com>
2020-07-24 00:00:15 +0800

    remove all files

---
+++ tools/linux_toolchain/lib/gcc/riscv64-unknown-elf/8.1.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/lhome/software/toolsbuild/slave/workspace/riscv64_build_elf_i386/build/../source/riscv/riscv-gcc/configure --target=riscv64-unknown-elf --with-mpc=/lhome/software/toolsbuild/slave/workspace/riscv64_build_elf_i386/lib-for-gcc-i386-linux/ --with-mpfr=/lhome/software/toolsbuild/slave/workspace/riscv64_build_elf_i386/lib-for-gcc-i386-linux/ --with-gmp=/lhome/software/toolsbuild/slave/workspace/riscv64_build_elf_i386/lib-for-gcc-i386-linux/ --prefix=/lhome/software/toolsbuild/slave/workspace/riscv64_build_elf_i386/install --disable-shared --disable-threads --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/lhome/software/toolsbuild/slave/workspace/riscv64_build_elf_i386/install/riscv64-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --src=../../source/riscv/riscv-gcc --enable-checking=yes --with-pkgversion='C-SKY RISCV Tools V1.2.2 B20190801' --enable-multilib --with-abi=lp64 --with-arch=rv64gcxcki 'CFLAGS_FOR_TARGET=-Os  -mcmodel=medany' 'CXXFLAGS_FOR_TARGET=-Os  -mcmodel=medany'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64" }, { "arch", "rv64gcxcki" } };
